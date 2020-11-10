410c201604637f98fe6c5309b2d45bbc1fafbe83
Krollbotid <37172495+Krollbotid@users.noreply.github.com>
2020-11-10 18:17:34 +0300

    Update task2.cpp

--- task2.cpp
+++ task2.cpp
@@ -23,3 +23,10 @@ int main()
     else
         cout<<"No";
 }
+/*python below
+odd = [0] * (int(input()) + 1)
+for i in [0] * int(input()):
+    a, b = map(int, input().split())
+    odd[a] = 1 - odd[a]
+    odd[b] = 1 - odd[b]
+print('Yes' if sum(odd) < 3 else 'No')
