3fe0e24f6801cdd25bfc4a00195003c1d2956087
CthulhuYar <49588738+CthulhuYar@users.noreply.github.com>
2020-11-11 11:02:32 +0300

    Update task 2.cpp

--- task 2.cpp
+++ task 2.cpp
@@ -1,32 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void dfs(int start, vector<bool>& color, const vector<vector<int>>& G, int n){
+    color[start] = true;
+    for(int v = 0; v < n; v++){
+        if(v != start && G[start][v] == 1 && !color[v])
+            dfs(v, color, G, n);
+    }
+}
+
 int main()
 {
     int N,M,nechet=0;
     cin>>N>>M;
     vector <int> counts(N,0);
+    vector<bool> color(N, false);
+    vector<vector<int>> G(N, vector<int>(N, 0));
     for (int i=0; i<M; i++)
     {
-       int a,b;
-       cin>>a>>b;
-       counts[a-1]++;
-       counts[b-1]++;
+        int a,b;
+        cin>>a>>b;
+        counts[a-1]++;
+        counts[b-1]++;
+        G[a - 1][b - 1] = 1;
+        G[b - 1][a - 1] = 1;
     }
     for (int i=0; i<N; i++)
     {
-       if(counts[i]%2==1)
-        nechet++;
+        if(counts[i]%2==1)
+            nechet++;
     }
-    if ((nechet==2)||(nechet==0))
+    int Ncomp = 0;
+    for(int i = 0; i < N; i++){
+        if (!color[i]){
+            Ncomp++;
+            dfs(i, color, G, N);
+        }
+    }
+    if (nechet <= 2 && Ncomp == 1)
         cout<<"Yes";
     else
         cout<<"No";
 }
-/*python below
-odd = [0] * (int(input()) + 1)
-for i in [0] * int(input()):
-    a, b = map(int, input().split())
-    odd[a] = 1 - odd[a]
-    odd[b] = 1 - odd[b]
-print('Yes' if sum(odd) < 3 else 'No')
